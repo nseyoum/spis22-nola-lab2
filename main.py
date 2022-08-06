9ac85167027fbfe3e180985e8f7a554eacdb4d30
Nola Seyoum <88351195+nseyoum@users.noreply.github.com>
2022-08-06 23:47:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# The goal of this program is to practice Python constructs
+def getNumber():
+
+   symbols = input("Enter a digit: ")
+
+   number = int(symbols)
+
+   return number
+  
+def sumTwo(a,b):
+
+   c = a + b
+
+   return c
+
+
+
+x = sumTwo(3,5)
+
+print(x)
